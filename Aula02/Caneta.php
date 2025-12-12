86276223b2006aad18528d3efdba4ea7414275a1
elenilsonguimaraes <guimaraes_elenilson@hotmail.com>
2025-12-12 15:05:26 -0300

    update

--- Aula02/Caneta.php
+++ Aula02/Caneta.php
@@ -1,12 +1,7 @@
 <?php
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
 /**
- * Description of Caneta
+ *AADescription of Caneta
  *
  * @author Guimarães
  */
